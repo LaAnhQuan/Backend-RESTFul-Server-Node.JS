2dd524b349152acc2d9c7d5f063cade147ca1f8b
La Anh Quan <laanhquan1807@gmail.com>
2025-03-31 00:37:11 +0700

    video 104

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express') //commonjs
 const configViewEngine = require('./config/viewEngine')
-const app = express()
-const port = process.env.PORT || 8888;//port => hartcode . uat .pord
-const hostname = process.env.HOST_NAME;
 const webRoutes = require('./routes/web')
 const apiRoutes = require('./routes/api')
 const fileUpload = require('express-fileupload');
 const connection = require('./config/database')
+const { MongoClient } = require('mongodb');
 
+
+const app = express()
+const port = process.env.PORT || 8888;//port => hartcode . uat .pord
+const hostname = process.env.HOST_NAME;
 //config fileUpload
 // default options
 app.use(fileUpload());
@@ -29,8 +31,22 @@ app.use('/v1/api/', apiRoutes);
 (async () => {
 
     try {
-        //test connection
-        await connection();
+        //using mongoose
+        // await connection();
+
+        //using mongodb driver
+        // Connection URL
+        const url = process.env.DB_HOST_WITH_DRIVER;
+        const client = new MongoClient(url);
+
+        // Database Name
+        const dbName = process.env.DB_NAME;
+
+        await client.connect();
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('documents');
+
         app.listen(port, hostname, () => {
             console.log(`Example app listening on port ${port}`)
         })
