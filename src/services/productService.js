364d99881c51cfc9230c67e280f51819578fdb4f
La Anh Quan <laanhquan1807@gmail.com>
2025-03-31 23:34:21 +0700

    Them moi User vao Project

--- src/services/productService.js
+++ src/services/productService.js
@@ -5,8 +5,24 @@ const Project = require('../models/project');
 const createEmptyProjectService = async (data) => {
 
     try {
-        let result = await Project.create(data)
-        return result
+        if (data.type === "EMPTY-PROJECT") {
+            let result = await Project.create(data)
+            return result
+        }
+        if (data.type === "ADD-USERS") {
+            let myProject = await Project.findById(data.projectId).exec();
+
+            for (let i = 0; i < data.usersArr.length; i++) {
+                myProject.usersInfor.push(data.usersArr[i]);
+            }
+
+            let newRusult = await myProject.save();
+
+
+            return newRusult
+
+        }
+        return null;
     } catch (error) {
         console.log(error);
         return null;
